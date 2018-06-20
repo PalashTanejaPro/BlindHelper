a9bf64f050e1737f15b023bff5b15928afccd1eb
erikaaatan <eatan18@gmail.com>
2018-06-20 11:34:52 -0400

    Minimize function calls

--- js/scan.js
+++ js/scan.js
@@ -1,57 +1,52 @@
 $(document).ready(function() {
- var sourceCode = document.getElementsByTagName('html')[0].innerHTML;
- var sideBarContent = "";
- var navigationContent = "";
- var headers = "";
- var paragraphContent = "";
- var lists = "";
- var tableContent = "";
- var imageAlt = "";
- var blockQuotes = "";
- var footers = "";
- var buttons = "";
- var cards = "";
- var carouselContent = "";
+  var sourceCode = document.getElementsByTagName('html')[0].innerHTML;
+  var sideBarContent = "";
+  var navigationContent = "";
+  var headers = "";
+  var paragraphContent = "";
+  var lists = "";
+  var tableContent = "";
+  var imageAlt = "";
+  var blockQuotes = "";
+  var footers = "";
+  var buttons = "";
+  var cards = "";
+  var carouselContent = "";
+  var tags = ["aside", "h", "p", "li", "th", "td", "a"];
+  var tagContent = [sideBarContent, headers, paragraphContent, lists, tableContent,
+                    tableContent, navigationContent];
+  var classes = ["nav", "breadcrumb-item", "tab-content", "page-link",
+                  "navigation", "content", "main", "list-group-item", "blockquote",
+                  "footer", "btn", "card", "carousel-caption"];
+  var classContent = [sideBarContent, sideBarContent, sideBarContent, sideBarContent,
+                      sideBarContent, paragraphContent, paragraphContent, lists,
+                      blockQuotes, footers, buttons, cards, carouselContent];
 
- function addToContent(content, tag) {
-    var count = document.getElementsByTagName(tag).length;
+  function addToContent(tag) {
+     var count = document.getElementsByTagName(tag).length;
+     var content = "";
+     for (var i = 0; i < count; i++) {
+       content += document.getElementsByTagName(tag)[i].innerHTML + " ";
+      }
+     return content;
+  }
+
+  function addToContentWithClass(htmlclass) {
+    var count = document.getElementsByClassName(htmlclass).length;
+    var content = "";
     for (var i = 0; i < count; i++) {
-      content += document.getElementsByTagName(tag)[i].innerHTML + " ";
-     }
-   console.log(content);
- }
-
- function addToContentWithClass(content, htmlclass) {
-   var count = document.getElementsByClassName(htmlclass).length;
-   for (var i = 0; i < count; i++) {
-     content += document.getElementsByClassName(htmlclass)[i].innerHTML + " ";
-   }
- }
-
- addToContent(sideBarContent, "aside");
- addToContentWithClass(sideBarContent, "nav");
- addToContentWithClass(sideBarContent, "breadcrumb-item");
- addToContentWithClass(sideBarContent, "tab-content");
- addToContentWithClass(sideBarContent, "page-link");
- addToContentWithClass(sideBarContent, "navigation");
- addToContent(headers, "h");
- addToContent(paragraphContent, "p");
- addToContentWithClass(paragraphContent, "content");
- addToContentWithClass(paragraphContent, "main");
- addToContent(lists, "li");
- addToContentWithClass(lists, "list-group-item");
- addToContent(tableContent, "th");
- addToContent(tableContent, "td");
- addToContent(navigationContent, "a");
- addToContentWithClass(blockQuotes, "blockquote");
- addToContentWithClass(footers, "footer");
- addToContentWithClass(buttons, "btn");
- addToContentWithClass(cards, "card");
- addToContentWithClass(carouselContent, "carousel-caption");
-
- var count = document.getElementsByTagName("img").length;
- for (var i = 0; i < count; i++) {
-   imageAlt += document.getElementsByTagName("img")[i].getAttribute("alt") + " ";
- }
-
+      content += document.getElementsByClassName(htmlclass)[i].innerHTML + " ";
+    }
+    return content;
+  }
+  for (var i = 0; i < tags.length; i++) {
+    tagContent[i] = addToContent(tags[i]);
+  }
+  for (var i = 0; i < classes.length; i++) {
+    classContent[i] = addToContentWithClass(classes[i]);
+  }
+  var count = document.getElementsByTagName("img").length;
+  for (var i = 0; i < count; i++) {
+    imageAlt += document.getElementsByTagName("img")[i].getAttribute("alt") + " ";
+  }
 });
